210023672ed4941ca024fd87a3c3e8dfb4dd4957
Eddie Wolff <ecwolff3@illinois.edu>
2022-09-09 15:44:01 -0500

    Fixed unterminated strings

--- MRMS_script_no_input.py
+++ MRMS_script_no_input.py
@@ -51,10 +51,10 @@ for i in range(0, int(timedelta_hours)+1, 1):
     os.system(f'mkdir {date_string}')
     file_name = f'{date_string}.zip'
     with ZipFile(file_name, 'r') as zip:
-        zip.extract(f'{date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_
+        zip.extract(f'{date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_\
         Pass1_00.00_{year}{month}{day}-{hour}0000.grib2.gz')
-    os.system(f'gunzip {date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_
+    os.system(f'gunzip {date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_\
     Pass1_00.00_{year}{month}{day}-{hour}0000.grib2.gz')
-    os.system(f'mv {date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_
+    os.system(f'mv {date_string}/CONUS/MultiSensor_QPE_01H_Pass1/MRMS_MultiSensor_QPE_01H_\
     Pass1_00.00_{year}{month}{day}-{hour}0000.grib2 grib_files')
     os.system(f'rm -r {date_string}')
